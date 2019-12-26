dde55b1d6ced3d5d05f4e72618529e032023aa36
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-12-26 18:09:31 +0200

    Updates

---
+++ precache-manifest.6be6ebb8d62b63c706ef0b1d5b461014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0925c757ded850d81a2752a008bbffae",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "851a2babdcefcea941f8",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "ea3d99cbcd12b6f28fee",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.9eb86b2d.chunk.js"
+  },
+  {
+    "revision": "851a2babdcefcea941f8",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.777feb74.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
